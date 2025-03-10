761b0dbf5267e2572c9fb319483a27462f32d42c
julsluks <julievillegas77@gmail.com>
2025-03-10 13:14:44 +0100

    Update index page title and button text for improved clarity

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="min-h-screen text-white flex flex-col items-center justify-center">
         <ConnectedLinesBackground />
-        <h1 class="text-4xl font-bold mb-4">Welcome to the Game Project</h1>
+        <h1 class="text-4xl font-bold mb-4">Welcome to the High Link</h1>
         <p class="text-lg mb-8">Get ready to play an amazing game!</p>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Start Game
+            Dowload Now
         </button>
     </div>
 </template>
